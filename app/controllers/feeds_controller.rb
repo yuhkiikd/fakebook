d532d30d9821782ed25c14b9d5b075d7a403a330
yuhkiikd <yuhki.ikd@gmail.com>
2019-09-08 08:20:32 +0000

    2019090801_文章投稿機能追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -77,6 +77,6 @@ class FeedsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
-      params.require(:feed).permit(:image, :image_cache)
+      params.require(:feed).permit(:image, :image_cache, :content)
     end
 end
---
+++ db/migrate/20190907073236_content_column_add_feeds.rb
@@ -0,0 +1,5 @@
+class ContentColumnAddFeeds < ActiveRecord::Migration[5.2]
+  def change
+    add_column :feeds, :content, :text
+  end
+end
