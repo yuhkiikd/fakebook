0fffef97d64b6b528bf7b586c45548458e5d875c
yuhkiikd <yuhki.ikd@gmail.com>
2019-09-09 00:42:00 +0000

    2019090901_ログイン・ログアウト機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user.id)
+      flash[:notice] = "ようこそ#{@user.name}さん"
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :feeds do
     collection do
       post :confirm
     end
   end
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
