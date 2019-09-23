e339defb64ceae27724375633efc3508851684db
yuhkiikd <yuhki.ikd@gmail.com>
2019-09-23 05:53:03 +0000

    201092305_デプロイ

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,6 @@ module FakebookApp
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
   end
 end
